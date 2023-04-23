b146505e91ede40bfb4de523e9796fe356d0450a
HarrisonBrookesBoon <harrison.m.brookes@gmail.com>
2023-04-23 17:07:22 +0000

    Add 'edit' functionality when viewing a moodboard the user has rights to

--- moodboard/urls.py
+++ moodboard/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.detail, name='detail'),
     path('create/', views.create_moodboard, name='create_moodboard'),
     path('detail/<int:pk>/', views.detail, name='detail'),
+    path('edit/<int:moodboard_id>/', views.edit_moodboard, name='edit_moodboard'),
     path('delete/<int:pk>/', views.delete_moodboard, name='delete_moodboard'),
 ]
--- moodboard/views.py
+++ moodboard/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render, redirect, get_object_or_404
 import cloudinary.uploader
 from django.db.models import Q
+from django.http import HttpResponseForbidden
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import MoodboardForm, ImageForm
 from .models import Moodboard, Image
 
+
 def create_moodboard(request):
     if request.method == 'POST':
         form = MoodboardForm(request.POST)
-        #image_form = ImageForm(request.POST, request.FILES)
 
         if form.is_valid():
             if request.FILES.getlist('image'):
                 moodboard = form.save(commit=False)  # Don't save the instance yet
                 moodboard.user = request.user  # Associate the logged-in user with the moodboard
-                moodboard = form.save() # Save the moodboard instance
+                moodboard = form.save()  # Save the moodboard instance
                 
                 for img in request.FILES.getlist('image'):
                     uploaded_image = cloudinary.uploader.upload(img)
@@ -24,22 +25,49 @@ def create_moodboard(request):
                     Image.objects.create(moodboard=moodboard, image=image_url)
                 return redirect('moodboard:index') 
             else:
-                #Display an error to the user if no images are uploaded
+                # Display an error to the user if no images are uploaded
                 messages.error(request, 'Please upload at least one image')
     else:
         form = MoodboardForm()
-        #image_form = ImageForm(prefix='image_form')
 
     context = {
         'form': form,
-        #'image_form': image_form,
+        # 'image_form': image_form,
     }
 
     return render(request, 'moodboard/create_moodboard.html', context)
 
+
+def edit_moodboard(request, moodboard_id):
+    moodboard = get_object_or_404(Moodboard, pk=moodboard_id)
+
+    if request.user == moodboard.user or request.user.is_staff:
+        if request.method == 'POST':
+            form = MoodboardForm(request.POST, instance=moodboard)
+            if form.is_valid():
+                form.save()
+
+                # Delete existing images
+                Image.objects.filter(moodboard=moodboard).delete()
+
+                # Upload new images
+                for img in request.FILES.getlist('image'):
+                    uploaded_image = cloudinary.uploader.upload(img)
+                    image_url = uploaded_image['secure_url']
+                    Image.objects.create(moodboard=moodboard, image=image_url)
+
+                messages.success(request, 'Moodboard has been updated.')
+                return redirect('moodboard:detail', pk=moodboard_id)
+        else:
+            form = MoodboardForm(instance=moodboard)
+        images = Image.objects.filter(moodboard=moodboard)
+        return render(request, 'moodboard/edit_moodboard.html', {'form': form, 'images': images, 'moodboard': moodboard})
+    else:
+        return HttpResponseForbidden("You don't have permission to edit this Moodboard.")
+
 @login_required
 def delete_moodboard(request, pk):
-    moodboard = get_object_or_404(Moodboard, pk=pk)
+    moodboard = get_object_or_404(Moodboard, pk=moodboard_is)
     
     if request.user == moodboard.user or request.user.is_staff:
         moodboard.delete()
@@ -47,7 +75,8 @@ def delete_moodboard(request, pk):
         return redirect('moodboard:index')
     else:
         messages.error(request, 'You do not have permission to delete this moodboard.')
-        return redirect('moodboard:detail', pk=pk)
+        return redirect('moodboard:detail', pk=moodboard_id)
+
 
 def get_queryset(request):
     query = request.GET.get('q')
@@ -61,9 +90,11 @@ def get_queryset(request):
         moodboards = Moodboard.objects.all()
     return moodboards
 
+
 def index(request):
     moodboards = get_queryset(request)
     return render(request, 'moodboard/index.html', {'moodboards': moodboards})
+
 
 def detail(request, pk):
     moodboard = Moodboard.objects.get(pk=pk)
